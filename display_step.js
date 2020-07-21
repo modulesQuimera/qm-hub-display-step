8e0d6cc4cf8f93818292e8520a156cec2e820b27
STRNEOH <strneoh@localhost.localdomain>
2020-07-21 16:12:41 -0300

    adding description

--- display_step.js
+++ display_step.js
@@ -15,17 +15,34 @@ module.exports = function(RED) {
             var exportMode = globalContext.get("exportMode");
             var currentMode = globalContext.get("currentMode");
             var command = {
-                "type": "processing_modular_V1.0",
-                "slot": 1,
-                "compare": {},
-                "method": "display_step",
-                "step": node.step,
-                "step_number": node.step_number
+                type: "processing_modular_V1_0",
+                slot: 1,
+                compare: {},
+                method: "display_step",
+                step_label: node.step,
+                step_number: parseInt(node.step_number),
+                get_output: {},
             }
             var file = globalContext.get("exportFile")
             var slot = globalContext.get("slot");
-            if(currentMode == "test"){file.slots[slot].jig_test.push(command)}
-            else{file.slots[slot].jig_error.push(command)}
+            if(!(slot === "begin" || slot === "end")){
+                if(currentMode == "test"){
+                    file.slots[slot].jig_test.push(command);
+                }
+                else{
+                    file.slots[slot].jig_error.push(command);
+                }
+            }
+            else{
+                if(slot === "begin"){
+                    file.slots[0].jig_test.push(command);
+                    // file.begin.push(command);
+                }
+                else{
+                    file.slots[3].jig_test.push(command);
+                    // file.end.push(command);
+                }
+            }
             globalContext.set("exportFile", file);
             // node.status({fill:"green", shape:"dot", text:"done"}); // seta o status pra waiting
             console.log(command)
